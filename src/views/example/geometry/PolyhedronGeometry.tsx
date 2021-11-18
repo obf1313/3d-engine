13f824ab3a2872cedc929cd231a8d5e8e9db15c1
obf1313 <oubaifan@163.com>
2021-11-18 12:12:17 +0800

    多面，二维圆环，形状，球。

--- src/views/example/geometry/PolyhedronGeometry.tsx
+++ src/views/example/geometry/PolyhedronGeometry.tsx
@@ -1,13 +1,145 @@
 /**
- * @description: 平面缓冲几何体
+ * @description: 多面缓冲几何体
+ * 多面体在三维空间中具有一些平面的立体图形。
+ * 这个类将一个顶点数组投射到一个球面上，之后将它们细分为所需的细节级别。
+ * 这个类由 DodecahedronGeometry、IcosahedronGeometry、OctahedronGeometry 和 TetrahedronGeometry 所使用，以生成它们各自的几何结构。
  * @author: cnn
  * @createTime: 2021/11/18 9:17
  **/
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  Color, WebGLRenderer, PolyhedronGeometry as TPolyhedronGeometry, MeshBasicMaterial, Mesh,
+  DoubleSide
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { getClientWidth, getClientHeight } from '@utils/CommonFunc';
+import { CameraType, initCamera, initScene, resetThreeConst, THREE_CONST } from '@utils/ThreeUtils';
+
+let polyhedronGeometry: any;
 
 const PolyhedronGeometry = () => {
-  return (
-    <></>
-  );
+  const [threeContainer, setThreeContainer] = useState<any>();
+  const [renderer, setRenderer] = useState<any>();
+  const [animationId, setAnimationId] = useState<number>();
+  useEffect(() => {
+    initMyScene();
+    return () => {
+      // 移除 resize 监听
+      window.removeEventListener('resize', onWindowResize);
+      // 重置全局变量
+      resetThreeConst();
+      polyhedronGeometry = null;
+    };
+  }, []);
+  useEffect(() => {
+    return () => {
+      // 移除 animation
+      if (animationId) {
+        window.cancelAnimationFrame(animationId);
+      }
+    };
+  }, [animationId]);
+  useEffect(() => {
+    if (threeContainer) {
+      initRenderer();
+    }
+  }, [threeContainer]);
+  useEffect(() => {
+    if (renderer) {
+      initControls();
+      window.addEventListener('resize', onWindowResize, false);
+    }
+  }, [renderer]);
+  // 初始化场景
+  const initMyScene = () => {
+    THREE_CONST.scene = initScene({
+      background: new Color(0xcce0ff)
+    });
+    THREE_CONST.camera = initCamera({
+      cameraType: CameraType.perspectiveCamera,
+      perspectiveParams: {
+        fov: 45,
+        aspect: getClientWidth() / (getClientHeight() - 60),
+        near: 1,
+        far: 100,
+      },
+      position: [0, 200, 200]
+    });
+    const threeContainer = document.getElementById('threeContainer') || document;
+    initPolyhedronGeometry();
+    setThreeContainer(threeContainer);
+  };
+  // 初始化 webgl 渲染器
+  const initRenderer = () => {
+    // antialias 执行抗锯齿，alpha canvas 包含 alpha (透明度)
+    const renderer = new WebGLRenderer({ antialias: true, alpha: true });
+    // 设置 alpha。合法参数是一个 0.0 到 1.0 之间的浮点数
+    renderer.setClearAlpha(0.2);
+    // 设置设备像素比。通常用于避免 HiDPI 设备上绘图模糊
+    renderer.setPixelRatio(window.devicePixelRatio);
+    // 将输出 canvas 的大小调整为 (width, height) 并考虑设备像素比，且将视口从(0, 0)开始调整到适合大小，将 updateStyle 设置为 false 以阻止对 canvas 的样式做任何改变。
+    renderer.setSize(getClientWidth(), getClientHeight() - 60);
+    setRenderer(renderer);
+    threeContainer.appendChild(renderer.domElement);
+  };
+  // 初始化轨道控制器
+  const initControls = () => {
+    const controls = new OrbitControls(THREE_CONST.camera, renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.maxPolarAngle = Math.PI * 0.5;
+    controls.minDistance = 2;
+    controls.maxDistance = 80;
+    controls.update();
+    animate();
+  };
+  // 生成一个 polyhedronGeometry 放入场景中
+  const initPolyhedronGeometry = () => {
+    const verticesOfCube = [
+      -1, -1, -1,
+      1, -1, -1,
+      1, 1, -1,
+      -1, 1, -1,
+      -1, -1, 1,
+      1, -1, 1,
+      1, 1, 1,
+      -1, 1, 1
+    ];
+    const indicesOfFaces = [
+      2, 1, 0, 0, 3, 2,
+      0, 4, 7, 7, 3, 0,
+      0, 1, 5, 5, 4, 0,
+      1, 2, 6, 6, 5, 1,
+      2, 3, 7, 7, 6, 2,
+      4, 5, 6, 6, 7, 4
+    ];
+    // vertices — 一个顶点Array（数组）：[1,1,1, -1,-1,-1, ... ]。
+    // indices — 一个构成面的索引Array（数组）， [0,1,2, 2,3,0, ... ]。
+    // radius — Float - 最终形状的半径。
+    // detail — Integer - 将对这个几何体细分多少个级别。细节越多，形状就越平滑。
+    const geometry = new TPolyhedronGeometry(verticesOfCube, indicesOfFaces, 6, 2);
+    const material = new MeshBasicMaterial({
+      color: 0x00ff00,
+      side: DoubleSide
+    });
+    polyhedronGeometry = new Mesh(geometry, material);
+    THREE_CONST.scene.add(polyhedronGeometry);
+  };
+  // 更新
+  const animate = () => {
+    const animationId = requestAnimationFrame(animate);
+    setAnimationId(animationId);
+    if (polyhedronGeometry) {
+      polyhedronGeometry.rotation.x += 0.01;
+      polyhedronGeometry.rotation.y += 0.01;
+    }
+    renderer.render(THREE_CONST.scene, THREE_CONST.camera);
+  };
+  // 监听拉伸浏览器事件
+  const onWindowResize = () => {
+    THREE_CONST.camera.aspect = getClientWidth() / (getClientHeight() - 60);
+    THREE_CONST.camera.updateProjectionMatrix();
+    renderer.setSize(getClientWidth(), getClientHeight() - 60);
+  };
+  return <div id="threeContainer" />;
 };
 export default PolyhedronGeometry;
--- src/views/example/geometry/ShapeGeometry.tsx
+++ src/views/example/geometry/ShapeGeometry.tsx
@@ -3,11 +3,128 @@
  * @author: cnn
  * @createTime: 2021/11/18 9:18
  **/
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+  Color, WebGLRenderer, ShapeGeometry as TShapeGeometry, MeshBasicMaterial, Mesh,
+  DoubleSide, Shape
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { getClientWidth, getClientHeight } from '@utils/CommonFunc';
+import { CameraType, initCamera, initScene, resetThreeConst, THREE_CONST } from '@utils/ThreeUtils';
+
+let shapeGeometry: any;
 
 const ShapeGeometry = () => {
-  return (
-    <></>
-  );
+  const [threeContainer, setThreeContainer] = useState<any>();
+  const [renderer, setRenderer] = useState<any>();
+  const [animationId, setAnimationId] = useState<number>();
+  useEffect(() => {
+    initMyScene();
+    return () => {
+      // 移除 resize 监听
+      window.removeEventListener('resize', onWindowResize);
+      // 重置全局变量
+      resetThreeConst();
+      shapeGeometry = null;
+    };
+  }, []);
+  useEffect(() => {
+    return () => {
+      // 移除 animation
+      if (animationId) {
+        window.cancelAnimationFrame(animationId);
+      }
+    };
+  }, [animationId]);
+  useEffect(() => {
+    if (threeContainer) {
+      initRenderer();
+    }
+  }, [threeContainer]);
+  useEffect(() => {
+    if (renderer) {
+      initControls();
+      window.addEventListener('resize', onWindowResize, false);
+    }
+  }, [renderer]);
+  // 初始化场景
+  const initMyScene = () => {
+    THREE_CONST.scene = initScene({
+      background: new Color(0xcce0ff)
+    });
+    THREE_CONST.camera = initCamera({
+      cameraType: CameraType.perspectiveCamera,
+      perspectiveParams: {
+        fov: 45,
+        aspect: getClientWidth() / (getClientHeight() - 60),
+        near: 1,
+        far: 100,
+      },
+      position: [0, 200, 200]
+    });
+    const threeContainer = document.getElementById('threeContainer') || document;
+    initShapeGeometry();
+    setThreeContainer(threeContainer);
+  };
+  // 初始化 webgl 渲染器
+  const initRenderer = () => {
+    // antialias 执行抗锯齿，alpha canvas 包含 alpha (透明度)
+    const renderer = new WebGLRenderer({ antialias: true, alpha: true });
+    // 设置 alpha。合法参数是一个 0.0 到 1.0 之间的浮点数
+    renderer.setClearAlpha(0.2);
+    // 设置设备像素比。通常用于避免 HiDPI 设备上绘图模糊
+    renderer.setPixelRatio(window.devicePixelRatio);
+    // 将输出 canvas 的大小调整为 (width, height) 并考虑设备像素比，且将视口从(0, 0)开始调整到适合大小，将 updateStyle 设置为 false 以阻止对 canvas 的样式做任何改变。
+    renderer.setSize(getClientWidth(), getClientHeight() - 60);
+    setRenderer(renderer);
+    threeContainer.appendChild(renderer.domElement);
+  };
+  // 初始化轨道控制器
+  const initControls = () => {
+    const controls = new OrbitControls(THREE_CONST.camera, renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.maxPolarAngle = Math.PI * 0.5;
+    controls.minDistance = 2;
+    controls.maxDistance = 80;
+    controls.update();
+    animate();
+  };
+  // 生成一个 shapeGeometry 放入场景中
+  const initShapeGeometry = () => {
+    const x = 0;
+    const y = 0;
+    const heartShape = new Shape();
+    heartShape.moveTo(x + 5, y + 5);
+    heartShape.bezierCurveTo(x + 5, y + 5, x + 4, y, x, y);
+    heartShape.bezierCurveTo(x - 6, y, x - 6, y + 7, x - 6, y + 7);
+    heartShape.bezierCurveTo(x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19);
+    heartShape.bezierCurveTo(x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7);
+    heartShape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y);
+    heartShape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5);
+    const geometry = new TShapeGeometry(heartShape);
+    const material = new MeshBasicMaterial({
+      color: 0xff0000,
+      side: DoubleSide
+    });
+    shapeGeometry = new Mesh(geometry, material);
+    THREE_CONST.scene.add(shapeGeometry);
+  };
+  // 更新
+  const animate = () => {
+    const animationId = requestAnimationFrame(animate);
+    setAnimationId(animationId);
+    if (shapeGeometry) {
+      shapeGeometry.rotation.x += 0.01;
+      shapeGeometry.rotation.y += 0.01;
+    }
+    renderer.render(THREE_CONST.scene, THREE_CONST.camera);
+  };
+  // 监听拉伸浏览器事件
+  const onWindowResize = () => {
+    THREE_CONST.camera.aspect = getClientWidth() / (getClientHeight() - 60);
+    THREE_CONST.camera.updateProjectionMatrix();
+    renderer.setSize(getClientWidth(), getClientHeight() - 60);
+  };
+  return <div id="threeContainer" />;
 };
 export default ShapeGeometry;
