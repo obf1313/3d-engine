e81d273958dac62ef564a7ea78685ddbb6dd62c0
obf1313 <oubaifan@163.com>
2022-02-22 17:04:09 +0800

    雪花特效。

--- src/views/example/mine/Merry.tsx
+++ src/views/example/mine/Merry.tsx
@@ -5,9 +5,9 @@
  **/
 import React, { useEffect, useState } from 'react';
 import {
-  Color, WebGLRenderer, Vector3, PMREMGenerator,
-  MathUtils, PlaneGeometry, TextureLoader, RepeatWrapping,
-  ACESFilmicToneMapping, LoadingManager,
+  Color, WebGLRenderer, Vector3, PMREMGenerator, MathUtils,
+  PlaneGeometry, TextureLoader, RepeatWrapping, ACESFilmicToneMapping, LoadingManager,
+  PointsMaterial, Points, AdditiveBlending, BufferGeometry, Float32BufferAttribute
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { getClientWidth, getClientHeight } from '@utils/CommonFunc';
@@ -18,12 +18,16 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import { Sky } from 'three/examples/jsm/objects/Sky';
 import { Water } from 'three/examples/jsm/objects/Water';
 import { LoadingOutlined } from '@ant-design/icons';
-import { Spin } from 'antd';
+import { Spin, Radio } from 'antd';
 import dayjs from 'dayjs';
+import { LightningStorm } from 'three/examples/jsm/objects/LightningStorm';
 
 let sun: any;
 let water: any;
 let ship: any;
+let points1: any;
+let points2: any;
+let sky: any;
 
 const Merry = () => {
   const [threeContainer, setThreeContainer] = useState<any>();
@@ -31,7 +35,7 @@ const Merry = () => {
   const [animationId, setAnimationId] = useState<number>();
   const [loadingProcess, setLoadingProcess] = useState<number>(0);
   const [sunHeight, setSunHeight] = useState<number>(0);
-  const [weatherList, setWeatherList] = useState<Array<string>>(['sun', 'lightStorm', 'rain', 'snow']);
+  const [currentWeather, setCurrentWeather] = useState<string>('sun');
   useEffect(() => {
     initMyScene();
     getSunHeight();
@@ -62,6 +66,9 @@ const Merry = () => {
       window.addEventListener('resize', onWindowResize, false);
     }
   }, [renderer]);
+  useEffect(() => {
+    changeWeather();
+  }, [currentWeather]);
   // 初始化场景
   const initMyScene = () => {
     THREE_CONST.scene = initScene({
@@ -101,7 +108,7 @@ const Merry = () => {
     const controls = new OrbitControls(THREE_CONST.camera, renderer.domElement);
     controls.maxPolarAngle = Math.PI * 0.45;
     controls.minDistance = 0.1;
-    controls.maxDistance = 2000;
+    controls.maxDistance = 100;
     controls.update();
     animate();
   };
@@ -123,30 +130,32 @@ const Merry = () => {
     });
     water.rotation.x = -Math.PI / 2;
     THREE_CONST.scene.add(water);
-    const sky = new Sky();
+    sky = new Sky();
     sky.scale.setScalar(100000);
     THREE_CONST.scene.add(sky);
     const skyUniforms = sky.material.uniforms;
+    // 不透明度
     skyUniforms['turbidity'].value = 10;
     skyUniforms['rayleigh'].value = 2;
+    // 平均系数
     skyUniforms['mieCoefficient'].value = 0.005;
     skyUniforms['mieDirectionalG'].value = 0.8;
+    updateSun(sunHeight);
+  };
+  const updateSun = (elevation: number) => {
     // elevation 越小越接近水平线，最高空 90
     const parameters = {
-      elevation: sunHeight,
+      elevation,
       azimuth: 180
     };
     const pmremGenerator = new PMREMGenerator(renderer);
-    const updateSun = () => {
-      const phi = MathUtils.degToRad(90 - parameters.elevation);
-      const theta = MathUtils.degToRad(parameters.azimuth);
-      sun.setFromSphericalCoords(1, phi, theta);
-      sky.material.uniforms['sunPosition'].value.copy(sun);
-      water.material.uniforms['sunDirection'].value.copy(sun).normalize();
-      // @ts-ignore
-      THREE_CONST.scene.environment = pmremGenerator.fromScene(sky).texture;
-    };
-    updateSun();
+    const phi = MathUtils.degToRad(90 - parameters.elevation);
+    const theta = MathUtils.degToRad(parameters.azimuth);
+    sun.setFromSphericalCoords(1, phi, theta);
+    water.material.uniforms['sunDirection'].value.copy(sun).normalize();
+    sky.material.uniforms['sunPosition'].value.copy(sun);
+    // @ts-ignore
+    THREE_CONST.scene.environment = pmremGenerator.fromScene(sky).texture;
   };
   // 导入模型
   const initModel = () => {
@@ -167,6 +176,21 @@ const Merry = () => {
       THREE_CONST.scene.add(ship);
     });
   };
+  // 雪花（雨的）动画
+  const pointsRender = () => {
+    if (points1) {
+      points1.position.y -= 0.2;
+      if (points1.position.y < -100) {
+        points1.position.y = 100;
+      }
+    }
+    if (points2) {
+      points2.position.y -= 0.2;
+      if (points2.position.y < -100) {
+        points2.position.y = 100;
+      }
+    }
+  };
   // 更新
   const animate = () => {
     const time = performance.now() * 0.002;
@@ -178,6 +202,7 @@ const Merry = () => {
     if (water) {
       water.material.uniforms['time'].value += 0.5 / 60.0;
     }
+    pointsRender();
     renderer.render(THREE_CONST.scene, THREE_CONST.camera);
   };
   // 监听拉伸浏览器事件
@@ -194,8 +219,72 @@ const Merry = () => {
     const leave6 = currentHour - 6;
     setSunHeight(leave6 * one);
   };
+  // 改变天气
+  const changeWeather = () => {
+    if (currentWeather === 'lightStorm') {
+      const lightingStorm = new LightningStorm({
+        size: 20,
+        minHeight: 100
+      });
+      THREE_CONST.scene.add(lightingStorm);
+    } else if (currentWeather === 'rain') {
+      updateSun(0);
+      addPoints('rain');
+    } else if (currentWeather === 'snow') {
+      updateSun(0);
+      addPoints('snow');
+    }
+  };
+  // 添加雨特效
+  const addPoints = (type: 'rain' | 'snow') => {
+    const texture = new TextureLoader().load(staticUrlPrefix + 'textures/snow.png');
+    const material = new PointsMaterial({
+      size: 2,
+      transparent: true,
+      opacity: 0.8,
+      map: texture,
+      // 在使用此材质显示对象时要使用何种混合。
+      blending: AdditiveBlending,
+      // 粒子的大小是否会被相机深度衰减，默认为 true（仅限透视相机）。
+      sizeAttenuation: true,
+      // 是否在渲染此材质时启用深度测试。
+      depthTest: false
+    });
+    const geometry: any = new BufferGeometry();
+    const vertices = [];
+    for (let i = 0; i < 2000; i++) {
+      const x = MathUtils.randFloatSpread(100);
+      const y = MathUtils.randFloatSpread(100);
+      const z = MathUtils.randFloatSpread(100);
+      let vertex: any = new Vector3(x, y, z);
+      // 纵向移动速度
+      vertex.velocityY = 0.1 + Math.random() / 15;
+      // 横向移动速度
+      vertex.velocityX = (Math.random() - 0.5) / 3;
+      // 将顶点加入几何
+      vertices.push(x, y, z);
+    }
+    geometry.setAttribute('position', new Float32BufferAttribute(vertices, 3));
+    points1 = new Points(geometry, material);
+    points2 = new Points(geometry, material);
+    points1.position.y = 100;
+    points2.position.y = 200;
+    THREE_CONST.scene.add(points1);
+    THREE_CONST.scene.add(points2);
+  };
+  // 天气列表
+  const weatherList: Array<string> = ['sun', 'lightStorm', 'rain', 'snow'];
   return (
-    <Spin spinning={loadingProcess < 100} indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}>
+    <Spin spinning={loadingProcess < 100} indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} style={{ position: 'relative' }}>
+      <div style={{ position: 'absolute', top: 20, left: 20 }}>
+        <Radio.Group
+          options={weatherList.map((item: string) => ({ label: item, value: item }))}
+          optionType="button"
+          buttonStyle="solid"
+          onChange={(e: any) => setCurrentWeather(e.target.value)}
+          value={currentWeather}
+        />
+      </div>
       <div id="threeContainer" />
     </Spin>
   );
